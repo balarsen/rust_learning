e1d500bf5fea0bc06a7435f855ae358b1682506a
Brian Larsen <balarsen@lanl.gov>
2017-07-16 12:18:45 -0800

    adder, good tests, we will now group them up in the next commit

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -25,6 +25,14 @@ fn add_two_test1() {
     assert_eq!(4, add_two(2));
 }
 
+// this is not run and shows up ignored in cargo test
+// unless cargo test -- --ignored which only tests the ignored ones
+#[test]
+#[ignore]
+fn expensive_test() {
+    // code that takes an hour to run
+}
+
 fn add_two(a: i32) -> i32 {
     a + 2
 }
