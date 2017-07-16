a0e0dea4e6169085cf9f6b4552585382a9a5a53d
Brian Larsen <balarsen@lanl.gov>
2017-07-16 11:47:15 -0800

    working concurency

--- philosophers/src/main.rs
+++ philosophers/src/main.rs
@@ -1,52 +1,62 @@
 use std::thread;
 use std::sync::{Mutex, Arc};
 
-struct Table {
-    forks: Vec<Mutex<()>>,
-}
-
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(150));
+
+        let _right = table.forks[self.right].lock().unwrap();
+
         println!("{} is eating.", self.name);
 
-        // thread::sleep_ms(1000); This is depricated by a compiler warning
         std::thread::sleep(std::time::Duration::from_millis(1000));
 
         println!("{} is done eating.", self.name);
     }
-}
-// without this above we would have to do
-// fn main() {
-//     let p1 = Philosopher { name: "Judith Butler".to_string() };
-//     let p2 = Philosopher { name: "Gilles Deleuze".to_string() };
-//     let p3 = Philosopher { name: "Karl Marx".to_string() };
-//     let p4 = Philosopher { name: "Emma Goldman".to_string() };
-//     let p5 = Philosopher { name: "Michel Foucault".to_string() };
-// }
 
+}
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
 
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
     let philosophers = vec![
-        Philosopher::new("Judith Butler"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Karl Marx"),
-        Philosopher::new("Emma Goldman"),
-        Philosopher::new("Michel Foucault"),
+        Philosopher::new("Judith Butler", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
     ];
 
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
 
