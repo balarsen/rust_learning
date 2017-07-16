85a7dcdf5145620006710430e573638606a94363
Brian Larsen <balarsen@lanl.gov>
2017-07-16 11:58:59 -0800

    intial typing, not quite there

--- embed/src/lib.rs
+++ embed/src/lib.rs
@@ -1,6 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+
+use std::thread;
+
+// don't let compiler magle the names
+#[no_mangle]
+pub extern "C" fn process() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..5_000_000 {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+        h.join().map_err(|_| "Could not join a thread!").unwrap());
     }
 }
