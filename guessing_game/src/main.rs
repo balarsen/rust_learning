fc4a73bb693eaa7ace56bf1da47bcf9c6b5bf2ca
Brian Larsen <balarsen@lanl.gov>
2017-07-16 11:15:03 -0800

    guessing: finished version

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,8 +9,9 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
+loop {
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -20,16 +21,21 @@ fn main() {
         .expect("Failed to read line");
 
     // convert the string to a u8
-    let guess: u8 = guess.trim().parse()
-        .ok()
-        .expect("Please type a number!");
+    let guess: u8 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
         Ordering::Less    => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+        Ordering::Equal   => {
+    println!("You win!");
+    break;
+}
     }
-
+}
 }
