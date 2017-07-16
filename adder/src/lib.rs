d9ee6848badecb719040e2acf85f58e517762e36
Brian Larsen <balarsen@lanl.gov>
2017-07-16 12:47:41 -0800

    adder: documentation, use cargo doc

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -53,15 +53,35 @@ mod tests {
     }
 }
 
+// seems like private function must be tested this way
+#[test]
+fn test_add_three() {
+    assert_eq!(5, add_three(2));
+}
+
+
 /// This function adds two to its argument.
 ///
 /// # Examples
 ///
-/// ```
+/// ```test
 /// use adder::add_two;
 ///
 /// assert_eq!(4, add_two(2));
 /// ```
+/// ```
+/// use adder::add_two;
+/// let x = add_two(5);
+/// ```
 pub fn add_two(a: i32) -> i32 {
-    a + 2
+    /// do the work
+    let b = a + 2;
+    return b;
+}
+
+
+/// This function adds three to its argument.
+///
+fn add_three(a: i32) -> i32 {
+    a + 3
 }
