425f000c9a2bbc2edddc3f51672ae48aa16412e5
Brian Larsen <balarsen@lanl.gov>
2017-07-16 11:09:08 -0800

    guessing: converts the string to a u8 and processes

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,15 @@
+extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -13,5 +19,17 @@ fn main() {
         .ok()
         .expect("Failed to read line");
 
+    // convert the string to a u8
+    let guess: u8 = guess.trim().parse()
+        .ok()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
+
 }
